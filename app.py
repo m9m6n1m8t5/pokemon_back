1271a8fa33bad77fb282354a5e12c66f6b3db3e4
m9m6n1m8t5 <m9m6n1m8t51080@gmail.com>
2022-04-28 15:35:24 +0900

    CORSエラー対策

--- app.py
+++ app.py
@@ -49,6 +49,15 @@ class Quiz(Resource):
 
 api.add_resource(Quiz, "/api/quiz")
 
+# CORSエラー対策
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
 @app.route("/", defaults={"path":""})
 @app.route("/<path:path>")
 def index(path):
